b03b68a819a13fd70f122b2a5803265d86e772e9
袁浩 <lucas@192.168.0.104>
2021-12-25 14:02:53 +0800

    create classes

---
+++ src/main/java/main.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class main {
+    private static final String username = "root";
+    private static final String password = "12345Aa?";
+    private static final String url ="jdbc:mysql://47.101.188.143:3306/dbpj";
+    public static void main(String[] args) {
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("注册驱动成功");
+        } catch (ClassNotFoundException e) {
+            System.out.println("注册驱动失败");
+            e.printStackTrace();
+            return ;
+        }
+        Connection conn=null;
+
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            ScriptRunner runner = new ScriptRunner(conn);
+
+        }catch(Exception e){
+            System.out.println("wrong");
+        }
+
+    }
+
+}
+
+
